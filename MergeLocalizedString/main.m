650749204319f5484676811576588991b00081f1
smalltask <smalltask@gmail.com>
2013-08-23 12:26:34 +0800

    Initial Commit

---
+++ MergeLocalizedString/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MergeLocalizedString
+//
+//  Created by  SmallTask on 13-8-23.
+//  Copyright (c) 2013年 cmf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
